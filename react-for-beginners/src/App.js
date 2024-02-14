2884e26f140b445598697acdba397130bff083a2
hanchangyu <hellohan95@gmail.com>
2024-02-14 20:01:12 +0900

    dep

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,27 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []); // []の中に何もないので基準がなくて一回だけ実行される
+  useEffect(() => {
+      console.log("I run when 'keyword' changes.");
+  }, [keyword]); // keywordの値が変わったら実行
+  useEffect(() => {
+      console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+      console.log("I run when 'counter and keyword' changes.");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!!</h1>
-      <Button text={"Continue"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}> CLick me</button>
     </div>
   );
 }
