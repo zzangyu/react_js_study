da69376992b4b96de6a5e9bbc9fa08f88e55ce04
hanchangyu <hellohan95@gmail.com>
2024-02-14 21:00:42 +0900

    cleanup - useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,27 +1,20 @@
 import { useEffect, useState } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destroyed");
+  }, []);
+  return <h1>Hello</h1>
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []); // []の中に何もないので基準がなくて一回だけ実行される
-  useEffect(() => {
-      console.log("I run when 'keyword' changes.");
-  }, [keyword]); // keywordの値が変わったら実行
-  useEffect(() => {
-      console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-      console.log("I run when 'counter and keyword' changes.");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev); 
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}> CLick me</button>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{ showing ? "Hide" : "Show" }</button>
     </div>
   );
 }
