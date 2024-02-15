81ef57abfcd680c692920897c83006d793593de7
hanchangyu <hellohan95@gmail.com>
2024-02-15 21:43:24 +0900

    coin tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,73 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const [inputValue, setInputValue] = useState(0);
+  const onChange = (event) => {
+    if (!isNaN(event.target.value)) {
+      setInputValue(event.target.value);
+    } else {
       return;
     }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
   };
-  console.log(toDos);
+
+  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedName, setSelectedName] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
+  const onSelect = (e) => {
+    if (e.target.value === "none") {
+      return;
+    }
+    var coinPrice = e.target.value.split("|")[0];
+    var coinName = e.target.value.split("|")[1];
+    setSelectedValue(coinPrice);
+    setSelectedName(coinName);
+    setIsSelected(true);
+  };
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do ..."
-        />
-        <button>Add To Do</button>
-        <hr />
-        <ul>
-          {toDos.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </form>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <h2>Write USD</h2>
+      <input
+        value={inputValue === 0 ? "" : inputValue}
+        onChange={onChange}
+        type="text"
+      />
+      <select onChange={onSelect}>
+        <option value={"none"}></option>
+        {coins.map((coin) => (
+          <option
+            value={coin.quotes.USD.price + "|" + coin.symbol}
+            key={coin.id}
+          >
+            {coin.name} ({coin.symbol})
+          </option>
+        ))}
+      </select>
+      <h2>{isSelected ? "" : "골라"}</h2>
+      <h2>{isSelected ? "USD ➡ " + selectedName : ""}</h2>
+      <h2>
+        {isSelected
+          ? inputValue +
+            "(USD) = " +
+            inputValue / selectedValue +
+            "(" +
+            selectedName +
+            ")"
+          : ""}
+      </h2>
     </div>
   );
 }
